3d54d447da468f3fbce213907979e637713572d4
Pravesh532 <praveshpatel9569@gmail.com>
2025-03-06 19:06:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
+	}
 
-class Student extends Person {}
+	// Getter for name
+	get name() {
+		return this._name;
+	}
 
-class Teacher extends Person {}
+	// Setter for name
+	set name(newName) {
+		if (typeof newName === "string" && newName.length > 0) {
+			this._name = newName;
+		} else {
+			console.log("Invalid name");
+		}
+	}
+
+	// Getter for age
+	get age() {
+		return this._age;
+	}
+
+	// Setter for age
+	set age(newAge) {
+		if (typeof newAge === "number" && newAge > 0) {
+			this._age = newAge;
+		} else {
+			console.log("Age must be a positive number");
+		}
+	}
+}
+
+class Student extends Person {
+	constructor(name, age) {
+		super(name, age);
+	}
+
+	study() {
+		console.log(`${this.name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	constructor(name, age) {
+		super(name, age);
+	}
+
+	teach() {
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
